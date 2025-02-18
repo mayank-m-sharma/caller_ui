2c327ae2936d0194defacf9a048533ca360deb47
Mayank Sharma <mayanksharmasav97@gmail.com>
2025-02-18 16:15:16 +0530

    remove axios import from Dialer component; update script source in index.html for new asset version

--- src/feature/dialer.tsx
+++ src/feature/dialer.tsx
@@ -20,7 +20,6 @@ import { cn } from '~/lib/utils';
 import { Button } from '~/components/ui/button';
 import { Input } from '~/components/ui/input';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
 
 interface Contact {
   id: string;
